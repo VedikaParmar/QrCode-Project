a149a40240a7b382cce447c83b91b40a6be57ec8
VedikaParmar <vedikaparmarvp25@gmail.com>
2025-06-02 12:06:20 +0530

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,19 @@
+import qrcode
+
+data = "https://g.co/kgs/xoMtcy1"
+
+code = qrcode.QRCode(
+    version=1,
+    error_correction= qrcode.constants.ERROR_CORRECT_L,
+    border=4,
+    box_size=10,
+    )
+
+code.add_data(data)
+code.make(fit=True)
+
+img = code.make_image(fill_color='black',back_color='white')
+
+img.save("OurQr.png")
+
+
